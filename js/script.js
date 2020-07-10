bfd3d6670b0da268758d2e0096dfa487f4244aff
Rohit Vishwakarma <rohitvish29@gmail.com>
2020-07-10 05:25:32 -0400

    pdf/social media/img overlay

--- js/script.js
+++ js/script.js
@@ -17,14 +17,14 @@ const cssBar=document.querySelector('.bar-css')
 const jsBar=document.querySelector('.bar-javascript')
 const reactBar=document.querySelector('.bar-react')
 
-var t1=new TimeLineLite()
+var t1=new TimelineLite()
 
-t1.fromTo(htmlBar, .75,{width:`calc(0%-6px)`},{width:`calc(90%-6px)`,ease: Power4.easeOut})
-    .fromTo(cssBar, .75,{width:`calc(0%-6px)`},{width:`calc(95%-6px)`,ease: Power4.easeOut})
-    .fromTo(jsBar, .75,{width:`calc(0%-6px)`},{width:`calc(75%-6px)`,ease: Power4.easeOut})
-    .fromTo(reactBar, .75,{width:`calc(0%-6px)`},{width:`calc(70%-6px)`,ease: Power4.easeOut})
+t1.fromTo(htmlBar, .75,{width:`calc(0% - 6px)`},{width:`calc(90% - 6px)`,ease: Power4.easeOut})
+    .fromTo(cssBar, .75,{width:`calc(0% - 6px)`},{width:`calc(80% - 6px)`,ease: Power4.easeOut})
+    .fromTo(jsBar, .75,{width:`calc(0% - 6px)`},{width:`calc(75% - 6px)`,ease: Power4.easeOut})
+    .fromTo(reactBar, .75,{width:`calc(0% - 6px)`},{width:`calc(90% - 6px)`,ease: Power4.easeOut})
 
-const controller=new ScrollMagic.Contoller()
+const controller=new ScrollMagic.Controller();
 const scene=new ScrollMagic.Scene({
     triggerElement:'.skills',
     triggerHook:0
@@ -33,8 +33,8 @@ const scene=new ScrollMagic.Scene({
 .addTo(controller)
 
 
-const showRequiredCategory=event=>{
-    const getId=event.getId
+function showRequiredCategory(event){
+    const getId=event.id
     const links=document.querySelectorAll('.work-category button')
     for(i=0;i<links.length;i++){
         if(links[i].hasAttribute('class')){
@@ -48,7 +48,7 @@ const showRequiredCategory=event=>{
     for(i=0;i<categories.length;i++){
         if(categories[i].hasAttribute('class')){
             categories[i].classList.remove('showCategory')
-            categories[i].classList.add(hideCategory)
+            categories[i].classList.add('hideCategory')
         }
     }
 
